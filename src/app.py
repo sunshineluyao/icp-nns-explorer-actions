d75d0a8041a8bd5fae42ee29cbb567605f469946
Luyao (Sunshine) Zhang <48966917+sunshineluyao@users.noreply.github.com>
2024-01-07 22:08:59 +0800

    readme

--- src/app.py
+++ src/app.py
@@ -8,25 +8,43 @@ import plotly.express as px
 url = "https://media.githubusercontent.com/media/sunshineluyao/icp-nns-db/main/data/proposals_no_empty.csv"
 df = pd.read_csv(url)
 
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+
+# Load the CSV data from the provided link
+url = "https://media.githubusercontent.com/media/sunshineluyao/icp-nns-db/main/data/proposals_no_empty.csv"
+df = pd.read_csv(url)
+
 # Initialize the Dash app
 app = dash.Dash(__name__)
-server = app.server
 
 # Create a bar plot using Plotly Express with a log-scale y-axis
-fig = px.bar(df['action'].value_counts(), x=df['action'].value_counts().index, y=df['action'].value_counts().values,labels={'x': 'Action', 'y': 'Count'}, title='Distribution of Actions')
-
+fig = px.bar(df['action'].value_counts(), x=df['action'].value_counts().index, y=df['action'].value_counts().values, labels={'x': 'Action', 'y': 'Count'}, title='Distribution of Actions')
 fig.update_yaxes(type="log")  # Set y-axis to logarithmic scale
 
 # Define the layout of the dashboard
 app.layout = html.Div([
+    # Header
+    html.H1("Internet Computer Protocol NNS Governance System Dashboard: Action"),
+    
+    # Paragraph with italicized notes
+    html.P([
+        "Choose the proposal status to view the distributions of proposal actions. ",
+        html.I("Notes: Action is the type of action the proposal seeks, such as propose, reject, or execute; Status is the current standing of the proposal, be it pending, accepted, negated, or unsuccessful.")
+    ]),
+
+    # Dropdown for status selection
     dcc.Dropdown(
         id='status-dropdown',
-        options=[
-            {'label': status, 'value': status} for status in df['status'].unique()
-        ],
+        options=[{'label': status, 'value': status} for status in df['status'].unique()],
         value=df['status'].unique()[0],  # Set default value to the first unique status
         multi=False
     ),
+
+    # Graph for displaying the data
     dcc.Graph(id='bar-plot')
 ])
 
